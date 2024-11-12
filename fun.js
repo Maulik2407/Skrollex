c847fe0a56e6164a5fd993f76d63c16f05345e6c
Mauliki solanki <mauliksolanki2407@gmail.com>
2024-11-11 21:51:46 -0800

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,15 @@
+var x = document.querySelector('.xbtn');
+var y = document.querySelector('.barbtn');
+
+function funone() {
+    x.style.display = "flex";
+    y.style.display = "none";
+}
+function funtwo() {
+    x.style.display = "none";
+    y.style.display = "flex";
+}
+
+
+var navbar= document.querySelector('.navbar');
+var mainscrol
